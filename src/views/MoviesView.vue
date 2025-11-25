7ee9bc388de70c872e4264f99bcaaa9f92fb4427
mateusramos123 <mateus.oliveira.ifc@gmail.com>
2025-11-25 14:49:00 -0300

    Categoria quase finalizada

--- src/views/MoviesView.vue
+++ src/views/MoviesView.vue
@@ -46,12 +46,8 @@ const formatDate = (date) => new Date(date).toLocaleDateString('pt-BR')
   <div class="container-pagina">
 
     <div class="categorias-container">
-      <button
-        v-for="genre in genreStore.genres"
-        :key="genre.id"
-        @click="listMovies(genre.id)"
-        :class="{ ativo: genre.id === genreStore.currentGenreId }"
-      >
+      <button v-for="genre in genreStore.genres" :key="genre.id" @click="listMovies(genre.id)"
+        :class="{ ativo: genre.id === genreStore.currentGenreId }">
         {{ genre.name }}
       </button>
     </div>
@@ -60,14 +56,18 @@ const formatDate = (date) => new Date(date).toLocaleDateString('pt-BR')
       <loading v-model:active="isLoading" is-full-page />
       <div class="lista-filmes">
         <div v-for="movie in movies" :key="movie.id" class="cartao-filme">
-          <img
-            :src="`https://image.tmdb.org/t/p/w500${movie.poster_path}`"
-            :alt="movie.title"
-            @click="openMovie(movie.id)"
-          />
+          <img :src="`https://image.tmdb.org/t/p/w500${movie.poster_path}`" :alt="movie.title"
+            @click="openMovie(movie.id)" />
           <div class="info-filme">
-            <h3>{{ movie.title }}</h3>
-            <p>{{ formatDate(movie.release_date) }}</p>
+            <p class="movie-title">{{ movie.title }}</p>
+            <p class="movie-release-date">{{ formatDate(movie.release_date) }}</p>
+            <p class="movie-genres">
+              <span v-for="genre_id in movie.genre_ids" :key="genre_id" @click="listMovies(genre_id)"
+                :class="{ active: genre_id === genreStore.currentGenreId }">
+                {{ genreStore.getGenreName(genre_id) }}
+              </span>
+            </p>
+
           </div>
         </div>
       </div>
@@ -76,32 +76,24 @@ const formatDate = (date) => new Date(date).toLocaleDateString('pt-BR')
 </template>
 
 <style scoped>
-body {
-  color: black;
-  font-family: 'Poppins', sans-serif;
-  margin: 0;
-}
+
 
 .container-pagina {
   display: flex;
   min-height: 100vh;
 }
 
-
-/* Categorias */
 .categorias-container {
-  margin: 1.5vw 0 0 0;
+  margin: 20px 0 0 0;
   width: 325px;
   background-color: #111;
   border-right: 1px solid #1f1f1f;
   display: flex;
   flex-direction: column;
   padding: 1.5rem;
-  scrollbar-width: thin;
-  scrollbar-color: #1A4ED8 #111;
   box-shadow: 2px 0 10px rgba(0, 0, 0, 0.6);
   border-radius: 0 20px 20px 0;
-  height: 140vh;
+  max-height: 1350px;
 }
 
 .categorias-container button {
@@ -110,70 +102,62 @@ body {
   color: #bbb;
   font-weight: 500;
   font-size: 1rem;
-  border-radius: 2rem;
-  padding: 0.7rem 1.2rem;
+  border-radius: 0.5rem;
+  padding: 0.8rem 1.2rem;
   margin-bottom: 1.5rem;
   cursor: pointer;
-  transition: all 0.25s ease-in-out;
   text-align: left;
-  letter-spacing: 0.3px;
+  letter-spacing: 0.5px;
 }
 
 .categorias-container button:hover {
-  color: #fff;
+  color: black;
   background: #1A4ED8;
-  transform: translateX(5px);
+  transform: translateX(3px);
   box-shadow: 0 0 12px #1A4ED8;
 }
 
 .categorias-container button.ativo {
   background: #1A4ED8;
-  color: #fff;
+  color: black;
   font-weight: 600;
 }
 
-/* Conteúdo */
+
 .area-conteudo {
   flex: 1;
   padding: 2rem 3rem;
-  overflow-y: auto;
 }
 
-/* Lista de filmes */
 .lista-filmes {
   display: grid;
   grid-template-columns: repeat(5, 1fr);
   gap: 2.2rem;
   justify-items: center;
 
-
 }
-
-
-
 
 .cartao-filme {
   background: #141414;
-  border-radius: 1rem;
+  border-radius: 0.3rem;
   overflow: hidden;
   transition: all 0.3s ease-in-out;
   cursor: pointer;
   box-shadow: 0 0 20px rgba(0, 0, 0, 0.8);
-  width: 250px;
-  height: 500px;
+  width: 260px;
+  height: 580px;
 }
 
 .cartao-filme img {
   width: 100%;
-  height: 410px;
+  height: 430px;
   object-fit: cover;
 }
 
 
 .info-filme {
-  padding: 1rem;
+  padding: 0rem;
   text-align: center;
-  background: #1b1b1b;
 }
 
 .info-filme h3 {
@@ -187,5 +171,53 @@ body {
 .info-filme p {
   font-size: 0.9rem;
   color: #b3b3b3;
+}
+
+.movie-details {
+  padding: 0 0.rem;
+}
+
+.movie-title {
+  font-size: 1.1rem;
+  font-weight: bold;
+  line-height: 1.3rem;
+  height: 2rem;
+}
+
+.movie-genres {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: flex-start;
+  justify-content: center;
+  gap: 0.2rem;
+  margin: 1vw 0 0 0;
+}
+
+.movie-genres span {
+  background-color: #1A4ED8;
+  border-radius: 0.5rem;
+  padding: 0.2rem 0.5rem;
+  color: #fff;
+  font-size: 0.8rem;
+  font-weight: bold;
+
+}
+
+.movie-genres span:hover {
+  cursor: pointer;
+  background-color: #1A4ED8;
+  box-shadow: 0 0 0.5rem #1A4ED8;
+}
+
+.active {
+  background-color: #1A4ED8;
+  font-weight: bolder;
+}
+
+.movie-genres span.active {
+  background-color: #1A4ED8;
+  color: #000;
+  font-weight: bolder;
 }
 </style>
